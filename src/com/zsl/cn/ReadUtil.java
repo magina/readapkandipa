626d742736885a1e2baf146f139c8f35e0b51d92
magina <starhf@gmail.com>
2017-06-16 09:50:49 +0800

    格式调整

--- src/com/zsl/cn/ReadUtil.java
+++ src/com/zsl/cn/ReadUtil.java
@@ -24,50 +24,69 @@ import com.dd.plist.PropertyListParser;
  * @author ZSL
  *
  */
-public final class ReadUtil {
+public final class ReadUtil 
+{
 	/**
 	 * ∂¡»°apk
 	 * @param apkUrl
 	 * @return
 	 */
-	public static Map<String,Object> readAPK(String apkUrl){
+	public static Map<String,Object> readAPK(String apkUrl)
+	{
 		ZipFile zipFile;
 		Map<String,Object> map = new HashMap<String, Object>();
 		try {
 			zipFile = new ZipFile(apkUrl);
 			Enumeration<?> enumeration = zipFile.entries();
 			ZipEntry zipEntry = null;
-			while (enumeration.hasMoreElements()) {
+			while (enumeration.hasMoreElements()) 
+			{
 				zipEntry = (ZipEntry) enumeration.nextElement();
-				if (zipEntry.isDirectory()) {
+				if (zipEntry.isDirectory()) 
+				{
 
-				} else {
-					if ("androidmanifest.xml".equals(zipEntry.getName().toLowerCase())) {
+				} 
+				else 
+				{
+					if ("androidmanifest.xml".equals(zipEntry.getName().toLowerCase())) 
+					{
 						AXmlResourceParser parser = new AXmlResourceParser();
 						parser.open(zipFile.getInputStream(zipEntry));
-						while (true) {
+						while (true) 
+						{
 							int type = parser.next();
-							if (type == XmlPullParser.END_DOCUMENT) {
+							if (type == XmlPullParser.END_DOCUMENT) 
+							{
 								break;
 							}
 							String name = parser.getName();
-							if(null != name && name.toLowerCase().equals("manifest")){
-								for (int i = 0; i != parser.getAttributeCount(); i++) {
-									if ("versionName".equals(parser.getAttributeName(i))) {
+							if(null != name && name.toLowerCase().equals("manifest"))
+							{
+								for (int i = 0; i != parser.getAttributeCount(); i++) 
+								{
+									if ("versionName".equals(parser.getAttributeName(i))) 
+									{
 										String versionName = getAttributeValue(parser, i);
-										if(null == versionName){
+										if(null == versionName)
+										{
 											versionName = "";
 										}
 										map.put("versionName", versionName);
-									} else if ("package".equals(parser.getAttributeName(i))) {
+									} 
+									else if ("package".equals(parser.getAttributeName(i))) 
+									{
 										String packageName = getAttributeValue(parser, i);
-										if(null == packageName){
+										if(null == packageName)
+										{
 											packageName = "";
 										}
 										map.put("package", packageName);
-									} else if("versionCode".equals(parser.getAttributeName(i))){
+									} 
+									else if("versionCode".equals(parser.getAttributeName(i)))
+									{
 										String versionCode = getAttributeValue(parser, i);
-										if(null == versionCode){
+										if(null == versionCode)
+										{
 											versionCode = "";
 										}
 										map.put("versionCode", versionCode);
@@ -81,50 +100,64 @@ public final class ReadUtil {
 				}
 			}
 			zipFile.close();
-		} catch (Exception e) {
+		} 
+		catch (Exception e) 
+		{
 			map.put("code", "fail");
 			map.put("error","∂¡»°apk ß∞‹");
 		}
 		return map;
 	}
 	
-	private static String getAttributeValue(AXmlResourceParser parser, int index) {
+	private static String getAttributeValue(AXmlResourceParser parser, int index) 
+	{
 		int type = parser.getAttributeValueType(index);
 		int data = parser.getAttributeValueData(index);
-		if (type == TypedValue.TYPE_STRING) {
+		if (type == TypedValue.TYPE_STRING) 
+		{
 			return parser.getAttributeValue(index);
 		}
-		if (type == TypedValue.TYPE_ATTRIBUTE) {
+		if (type == TypedValue.TYPE_ATTRIBUTE) 
+		{
 			return String.format("?%s%08X", getPackage(data), data);
 		}
-		if (type == TypedValue.TYPE_REFERENCE) {
+		if (type == TypedValue.TYPE_REFERENCE) 
+		{
 			return String.format("@%s%08X", getPackage(data), data);
 		}
-		if (type == TypedValue.TYPE_FLOAT) {
+		if (type == TypedValue.TYPE_FLOAT) 
+		{
 			return String.valueOf(Float.intBitsToFloat(data));
 		}
-		if (type == TypedValue.TYPE_INT_HEX) {
+		if (type == TypedValue.TYPE_INT_HEX) 
+		{
 			return String.format("0x%08X", data);
 		}
-		if (type == TypedValue.TYPE_INT_BOOLEAN) {
+		if (type == TypedValue.TYPE_INT_BOOLEAN) 
+		{
 			return data != 0 ? "true" : "false";
 		}
-		if (type == TypedValue.TYPE_DIMENSION) {
+		if (type == TypedValue.TYPE_DIMENSION) 
+		{
 			return Float.toString(complexToFloat(data)) + DIMENSION_UNITS[data & TypedValue.COMPLEX_UNIT_MASK];
 		}
-		if (type == TypedValue.TYPE_FRACTION) {
+		if (type == TypedValue.TYPE_FRACTION) 
+		{
 			return Float.toString(complexToFloat(data)) + FRACTION_UNITS[data & TypedValue.COMPLEX_UNIT_MASK];
 		}
-		if (type >= TypedValue.TYPE_FIRST_COLOR_INT && type <= TypedValue.TYPE_LAST_COLOR_INT) {
+		if (type >= TypedValue.TYPE_FIRST_COLOR_INT && type <= TypedValue.TYPE_LAST_COLOR_INT) 
+		{
 			return String.format("#%08X", data);
 		}
-		if (type >= TypedValue.TYPE_FIRST_INT && type <= TypedValue.TYPE_LAST_INT) {
+		if (type >= TypedValue.TYPE_FIRST_INT && type <= TypedValue.TYPE_LAST_INT) 
+		{
 			return String.valueOf(data);
 		}
 		return String.format("<0x%X, type 0x%02X>", data, type);
 	}
 
-	private static String getPackage(int id) {
+	private static String getPackage(int id) 
+	{
 		if (id >>> 24 == 1) {
 			return "android:";
 		}
@@ -132,7 +165,8 @@ public final class ReadUtil {
 	}
 
 	// ///////////////////////////////// ILLEGAL STUFF, DONT LOOK :)
-	public static float complexToFloat(int complex) {
+	public static float complexToFloat(int complex) 
+	{
 		return (float) (complex & 0xFFFFFF00) * RADIX_MULTS[(complex >> 4) & 3];
 	}
 
@@ -143,7 +177,8 @@ public final class ReadUtil {
 	/**
 	 * ∂¡»°ipa
 	 */
-	public static Map<String,Object> readIPA(String ipaURL){
+	public static Map<String,Object> readIPA(String ipaURL)
+	{
 		Map<String,Object> map = new HashMap<String,Object>();
 		try {
 			File file = new File(ipaURL);
@@ -151,14 +186,18 @@ public final class ReadUtil {
             ZipInputStream zipIns = new ZipInputStream(is);
             ZipEntry ze;
             InputStream infoIs = null;
-            while ((ze = zipIns.getNextEntry()) != null) {
-                if (!ze.isDirectory()) {
+            while ((ze = zipIns.getNextEntry()) != null) 
+            {
+                if (!ze.isDirectory()) 
+                {
                     String name = ze.getName();
-                    if (null != name && name.toLowerCase().contains("info.plist")) {
+                    if (null != name && name.toLowerCase().contains("info.plist")) 
+                    {
                         ByteArrayOutputStream _copy = new ByteArrayOutputStream();
                         int chunk = 0;
                         byte[] data = new byte[1024];
-                        while(-1!=(chunk=zipIns.read(data))){
+                        while(-1!=(chunk=zipIns.read(data)))
+                        {
                             _copy.write(data, 0, chunk);
                         }
                         infoIs = new ByteArrayInputStream(_copy.toByteArray());
@@ -198,17 +237,20 @@ public final class ReadUtil {
 	}
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("======apk=========");
 		String apkUrl = "apps/shenmiaotaowang_966.apk";
 		Map<String,Object> mapApk = ReadUtil.readAPK(apkUrl);
-		for (String key : mapApk.keySet()) {
+		for (String key : mapApk.keySet()) 
+		{
 			System.out.println(key + ":" + mapApk.get(key));
 		}
 		System.out.println("======ipa==========");
 		String ipaUrl = "apps/150211092729.ipa";
 		Map<String,Object> mapIpa = ReadUtil.readIPA(ipaUrl);
-		for (String key : mapIpa.keySet()) {
+		for (String key : mapIpa.keySet()) 
+		{
 			System.out.println(key + ":" + mapIpa.get(key));
 		}
 	}
